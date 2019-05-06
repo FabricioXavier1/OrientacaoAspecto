959cd2ad51ea42fe7eaed217e646c2cc990b0509
Fabricio Xavier <fabricio.xavier@squadra.com.br>
2019-05-06 08:57:19 -0300

    commit inicial

---
+++ OrientacaoAspecto/CacheAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+using PostSharp.Serialization;
+
+
+namespace OrientacaoAspecto
+{
+    [PSerializable]
+    public sealed class CacheAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            // Constrói o ID de cache.
+            var stringBuilder = new StringBuilder();
+            AppendCallInformation(args, stringBuilder);
+            var cacheKey = stringBuilder.ToString();
+
+            // Obtém o valor do cache.
+            var cachedValue = MemoryCache.Default.Get(cacheKey);
+
+            if (cachedValue != null)
+            {
+                // Se caso o valor já exista no cache, nem executa o método. Retorno o valor do cache imediatamente.
+                args.ReturnValue = cachedValue;
+                args.FlowBehavior = FlowBehavior.Return;
+            }
+            else
+            {
+                // Se o valor não está no cache, mantenha a execução do método, mas salve no cache para que possa ser utilizado no futuro.
+                args.MethodExecutionTag = cacheKey;
+                args.FlowBehavior = FlowBehavior.Continue;
+            }
+        }
+
+        public override void OnSuccess(MethodExecutionArgs args)
+        {
+            var cacheKey = (string)args.MethodExecutionTag;
+            MemoryCache.Default[cacheKey] = args.ReturnValue;
+        }
+
+        private static void AppendCallInformation(MethodExecutionArgs args, StringBuilder stringBuilder)
+        {
+            // Append tipo e nome do método.
+            var declaringType = args.Method.DeclaringType;
+            Formatter.AppendTypeName(stringBuilder, declaringType);
+            stringBuilder.Append('.');
+            stringBuilder.Append(args.Method.Name);
+
+            // Append generic arguments.
+            if (args.Method.IsGenericMethod)
+            {
+                var genericArguments = args.Method.GetGenericArguments();
+                Formatter.AppendGenericArguments(stringBuilder, genericArguments);
+            }
+
+            // Append argumentos.
+            Formatter.AppendArguments(stringBuilder, args.Arguments);
+        }
+    }
+}
---
+++ OrientacaoAspecto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrientacaoAspecto
+{
+    internal class Program
+    {
+        private static readonly Stopwatch stopwatch = Stopwatch.StartNew();
+
+        private static void Main(string[] args)
+        {
+            // Primeira chamada do método, nesse ponto, será lento.
+            WriteMessage(Hello("world"));
+            WriteMessage(Hello("universe"));
+
+            // Segunda chamada do método. O resultado já estará em cache
+            WriteMessage(Hello("world"));
+            WriteMessage(Hello("universe"));
+            Console.ReadKey();
+        }
+
+        // Escreve uma mensagem no console contando o tempo
+        private static void WriteMessage(string message)
+        {
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms - {message}");
+        }
+
+        [Cache]
+        private static string Hello(string who)
+        {
+            // Write something to the console and wait to show that the method is actually being executed.
+            WriteMessage(string.Format($"fazendo alguma coisa {who}."));
+            Thread.Sleep(5000);
+            
+            return string.Format($"Hello, {who}");
+        }
+    }
+}
